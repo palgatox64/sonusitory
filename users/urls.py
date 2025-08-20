c67f8c3ffa8783c88c357fad15509544f4d27a26
palgatox64 <se.navarrete@hotmail.com>
2025-08-20 14:11:15 -0400

    feat(auth): Enable redirect for authenticated users on login

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
-
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
